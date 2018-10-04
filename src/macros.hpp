87fa999a64b540f1134fb1466d3bbad8efbcbd06
Joshua Wise <joshuathomaswise@gmail.com>
2018-10-04 12:02:13 -0400

    fixed for node < 8.10

--- src/macros.hpp
+++ src/macros.hpp
@@ -34,6 +34,13 @@ void ThrowRangeError(NODE_ARGUMENTS info, const char* message) { ThrowException(
 #define fourth() 3
 #define fifth() 4
 
+#if defined(V8_MAJOR_VERSION) && (V8_MAJOR_VERSION > 6 ||                      \
+  (V8_MAJOR_VERSION == 6 && defined(V8_MINOR_VERSION) && V8_MINOR_VERSION >= 2))
+#define EXTRACT_STRING(isolate, string) isolate, string
+#else
+#define EXTRACT_STRING(_unused, string) string
+#endif
+
 #define UseValue int64_t value = node::ObjectWrap::Unwrap<Integer>(info.This())->value
 #define UseArgument\
 	if (info.Length() == 0) return ThrowTypeError(info, "Missing argument");\
